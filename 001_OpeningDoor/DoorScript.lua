0f9300ca61fa8cd12482e21b9a8cce8166c72fef
denkodin <denkodin@gmail.com>
2020-02-23 22:45:29 +0300

    Added files for the video "How to make an opening door in Roblox Studio" at the channel "DenCoder"

---
+++ 001_OpeningDoor/DoorScript.lua
@@ -0,0 +1,21 @@
+local Hinge = script.Parent.PrimaryPart
+local opened = false
+
+function OpenDoor()
+	if opened == false then
+		opened = true
+		for i = 1, 21 do
+			script.Parent:SetPrimaryPartCFrame(Hinge.CFrame*CFrame.Angles(0, math.rad(5), 0))
+			wait()
+		end
+	else
+		opened = false
+		for i = 1, 21 do
+			script.Parent:SetPrimaryPartCFrame(Hinge.CFrame*CFrame.Angles(0, math.rad(-5), 0))
+			wait()
+		end
+	end
+end
+
+script.Parent.Handle1.ClickDetector.MouseClick:Connect(OpenDoor)
+script.Parent.Handle2.ClickDetector.MouseClick:Connect(OpenDoor)
