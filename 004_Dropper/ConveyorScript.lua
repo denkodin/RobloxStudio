7a8ece9ef771aed47233b00876590d02c2292305
Denkodin <denkodin@gmail.com>
2020-03-08 02:49:11 +0300

    Added files for the video "How to make a dropper in Roblox Studio" at the channel "How to make in Roblox Studio"

---
+++ 004_Dropper/ConveyorScript.lua
@@ -0,0 +1,3 @@
+local Speed = 25
+
+script.Parent.ConveyorBelt.Velocity = script.Parent.ConveyorBelt.CFrame.LookVector * Speed
---
+++ 004_Dropper/DropperScript.lua
@@ -0,0 +1,15 @@
+local Drop = script.Parent.Drop
+
+function MakeBlock()
+	local Block = Instance.new("Part", game.Workspace)
+	Block.CFrame = Drop.CFrame
+	Block.Size = Vector3.new(2, 1, 2)
+	Block.Material = Enum.Material.Concrete
+	Block.BrickColor = BrickColor.new("White")
+	game.Debris:AddItem(Block, 5)
+end
+
+while true do
+	wait(1)
+	MakeBlock()
+end
