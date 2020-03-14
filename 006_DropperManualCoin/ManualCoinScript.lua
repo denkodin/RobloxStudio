fe6ea60bf33ba9db542a0e08c04a746768fe77cf
Denkodin <denkodin@gmail.com>
2020-03-14 17:22:02 +0300

    Added files for the video "How to make a dropper with coin and other items in Roblox Studio" at the channel "How to make in Roblox Studio"

---
+++ 006_DropperManualCoin/ManualCoinScript.lua
@@ -0,0 +1,19 @@
+local Template = script.Parent.DropPart:GetChildren()[1]:Clone()
+local Button = script.Parent.Button
+local Drop = script.Parent.Drop
+local Ready = true
+
+script.Parent.DropPart:GetChildren()[1]:Remove()
+
+function MakeBlock()
+	if Ready then
+		Ready = false
+		local Block = Template:Clone()
+		Block.Parent = game.Workspace
+		Block.CFrame = Drop.CFrame
+		game.Debris:AddItem(Block, 15)
+		wait(0.1)
+		Ready = true
+	end
+end
+Button.ClickDetector.MouseClick:Connect(MakeBlock)
