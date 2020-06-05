7c81ec9117faf5342c17d65c40545b370b67e136
Denkodin <denkodin@gmail.com>
2020-06-05 19:47:16 +0300

    Added files for the video "How to make money per mile in Roblox Studio" at the channel "How to make in Roblox Studio"

---
+++ 012_MoneyPerMile/CashDrivingScript.lua
@@ -0,0 +1,83 @@
+local ChangeTitlesEvent = game.ReplicatedStorage:WaitForChild("ChangeTitles")
+local Units = { {name = 'STUDS', coeff = 1},
+				{name = 'KM', coeff = 4000},
+				{name = 'MILES', coeff = 6400} }
+local Unit = 3
+local CashPerUnit = 250
+local TimeIncrement = 0.5
+local CashPerSecond = 1
+local CurrencyName = "$"
+local ControlCarOwner = true
+
+local cashPerStud = 1 / Units[Unit].coeff * CashPerUnit
+
+function getPlayerCarSeat(player)
+	local character = player.Character
+	if character then
+		local humanoid = character:FindFirstChild("Humanoid")
+		if humanoid then
+			local carName = player.Name .. "sCar"
+			if humanoid.SeatPart and humanoid.SeatPart:IsA("VehicleSeat") and humanoid.SeatPart.Parent then
+				return (not ControlCarOwner or humanoid.SeatPart.Parent.Name == carName) and humanoid.SeatPart or nil
+			end
+		end
+	end
+	return nil
+end
+		
+local driversCounters = {}
+
+ChangeTitlesEvent.OnServerEvent:Connect(function(player)
+	local counters = driversCounters[player.UserId]
+	if counters ~= nil then
+		counters.unit = counters.unit + 1
+		if counters.unit > #Units then
+			counters.unit = 1
+		end
+		ChangeTitlesEvent:FireClient(player, player, counters.duration, Units[counters.unit], counters.totalStuds, counters.cash)
+	end
+end)
+
+while true do
+	for i, player in pairs(game.Players:GetPlayers()) do
+		local seat = getPlayerCarSeat(player)
+		if seat ~= nil then
+			local counters = driversCounters[player.UserId]
+			if counters == nil then
+				driversCounters[player.UserId] = {totalStuds = 0, cash = 0, oldCash = 0, duration = 0, unit = Unit}
+				counters = driversCounters[player.UserId]
+				if counters == nil then
+					continue
+				end
+			end
+			counters.duration = counters.duration + TimeIncrement
+			counters.totalStuds = counters.totalStuds + (seat.Velocity.Magnitude >= 0.1 and seat.Velocity.Magnitude or 0) * TimeIncrement
+			counters.cash = counters.totalStuds * cashPerStud + counters.duration * CashPerSecond
+			local cashDelta = math.floor(counters.cash - counters.oldCash)
+			if cashDelta >= 1 then
+				player.leaderstats[CurrencyName].Value = player.leaderstats[CurrencyName].Value + cashDelta 
+				counters.oldCash = counters.oldCash + cashDelta 
+			end
+			local clonedScreen = player.PlayerGui:FindFirstChild("CashDrivingScreen")
+			if clonedScreen == nil then
+				clonedScreen = script.CashDrivingScreen:Clone()
+				if clonedScreen == nil then
+					continue
+				end
+				clonedScreen.Parent = player.PlayerGui
+			end
+			ChangeTitlesEvent:FireClient(player, counters.duration, Units[counters.unit], counters.totalStuds, counters.cash)
+		else
+			local counters = driversCounters[player.UserId]
+			if counters ~= nil then
+				driversCounters[player.UserId] = nil
+			end
+			local clonedScreen = player.PlayerGui:FindFirstChild("CashDrivingScreen")
+			if clonedScreen ~= nil then
+				clonedScreen:Remove()
+			end
+		end
+	end
+	wait(TimeIncrement)
+end
+
---
+++ 012_MoneyPerMile/LocalCashDriving.lua
@@ -0,0 +1,27 @@
+local ChangeTitlesEvent = game.ReplicatedStorage:WaitForChild("ChangeTitles")
+script.Parent:TweenPosition(UDim2.new(0.635, 0, 0.005, 0))
+
+function comma_value(n)
+	local left, num, right = string.match(n, '^([^%d]*%d)(%d*)(.-)$')
+	return left .. (num:reverse():gsub('(%d%d%d)', '%1 '):reverse()) .. right
+end
+
+function toHMS(seconds)
+	local h = math.floor(seconds / 3600)
+	local m = math.floor(seconds / 60) % 60
+	local s = seconds % 60
+	
+	return string.format("%02d:%02d:%02d", h, m, s)
+end
+
+ChangeTitlesEvent.OnClientEvent:Connect(function(Time, Unit, Distance, Cash)
+	local scale = Unit.coeff
+	script.Parent.CashLabel.CashValue.Text = comma_value(math.floor(Cash))
+	script.Parent.MileageLabel.MileageName.Text = Unit.name
+	script.Parent.MileageLabel.MileageValue.Text = math.floor(Distance / scale * 100) / 100
+	script.Parent.TimeLabel.TimeValue.Text = toHMS(Time)
+end)
+
+script.Parent.MileageLabel.ChangeUnitButton.MouseButton1Down:Connect(function()
+	ChangeTitlesEvent:FireServer()
+end)
