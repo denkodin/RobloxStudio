565445ee48e24c9808e4f3b9b9a6c9ede3ac1aa8
Denkodin <denkodin@gmail.com>
2020-12-30 02:12:44 +0300

    Added the feature multiple dealership for the video "How to make a car dealership in Roblox Studio - Part 3" at the channel "How to make in Roblox Studio"

---
+++ 011_DealershipPart3/features/MultipleDealership/Open.lua
@@ -0,0 +1,26 @@
+local Detector = script:WaitForChild("Dealership").Value:WaitForChild("Detector")
+local CloseDistance = math.max(Detector.Size.X, Detector.Size.Z)
+local CloseButton = script.Parent.Close
+
+Detector.Touched:Connect(function(hit)
+	if game.Players:GetPlayerFromCharacter(hit.Parent) == game.Players.LocalPlayer then
+		script.Parent.Enabled = true
+	end
+end)
+
+CloseButton.MouseButton1Down:Connect(function()
+	script.Parent.Enabled = false
+end)
+
+script.Parent:GetPropertyChangedSignal("Enabled"):Connect(function()
+	while script.Parent.Enabled do
+		wait(0.1)
+		if game.Players.LocalPlayer.Character then
+			local HumanoidRootPart = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+			local mag = (HumanoidRootPart.Position-Detector.Position).Magnitude
+			if mag >= CloseDistance + math.max(HumanoidRootPart.Size.X, HumanoidRootPart.Size.Z) and script.Parent.Enabled == true then
+				script.Parent.Enabled = false
+			end
+		end
+	end
+end)
